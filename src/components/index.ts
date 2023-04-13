1996fa28a54682e851b6d10f05e0004eed1c2ebe
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-04-13 15:20:30 -0300

    Exporting components through index file

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import ParticipantInput from "./ParticipantInput";
+import ParticipantList from "./ParticipantList";
+import Participant from "./Participant";
+
+export {
+  Header,
+  ParticipantInput,
+  ParticipantList,
+  Participant
+};
