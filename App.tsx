21c2492adf2aa9bd50fe791dacaf1481b5277b62
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-04-13 15:23:12 -0300

    Updating main App file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
-export default function App() {
+import { Event } from "./src/screens";
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Event />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
