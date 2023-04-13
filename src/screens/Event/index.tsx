4db1b36c0056d12dfaa0c040344ad0a72d38e1c3
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-04-13 15:21:58 -0300

    Adding Event screen

---
+++ src/screens/Event/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Header,
+  ParticipantInput,
+  ParticipantList
+} from "../../components";
+
+import styles from "./style";
+
+function Event() {
+  return (
+    <>
+      <Header />
+      <ParticipantInput />
+      <ParticipantList />
+    </>
+  );
+}
+
+export default Event;
