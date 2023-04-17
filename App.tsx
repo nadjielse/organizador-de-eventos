fdcbc4d9a2b8d81cb2b930449e14dda840ac11a3
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-04-17 11:07:02 -0300

    Adding body styles in the main App file

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Constants from "expo-constants";
 
 import { Event } from "./src/screens";
 
 function App() {
   return (
-    <View>
+    <View style={ styles.body }>
       <Event />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    paddingHorizontal: 12.5,
+    backgroundColor: "#131016"
+  }
+})
 
 export default App;
